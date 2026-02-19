753db1d58a195dc592ed709e2fa7c1b270e4ea32
Leela2305 <leelavyshnavi07@gmail.com>
2026-02-19 20:49:00 +0530

    Create Sum of two 2D Arrays.c

---
+++ Sum of two 2D Arrays.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int a[50][50], b[50][50], sum[50][50];
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    // Input first matrix
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Compute sum
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Print result
+    printf("Sum of two matrices:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
